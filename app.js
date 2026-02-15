9610da5e6283fa18be1aa5566a8eb7bf91a1dc3c
Dharani Preetha Kannan <dharanipreethakannan@gmail.com>
2026-02-16 04:56:48 +0530

    Use DATABASE_URL for Render Postgres

--- app.js
+++ app.js
@@ -14,11 +14,9 @@ const pgSession = require('connect-pg-simple')(session);
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // Required for Render Postgres
+    rejectUnauthorized: false
   }
 });
-
-module.exports = pool;
 
 
 // ---------------- Middleware ---------------- //
