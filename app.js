8b119193b99678413304bce4882b312438d11443
Dharani Preetha Kannan <dharanipreethakannan@gmail.com>
2026-02-16 22:13:25 +0530

    Use DATABASE_URL for Render Postgres

--- app.js
+++ app.js
@@ -13,12 +13,8 @@ const pgSession = require('connect-pg-simple')(session);
 
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,       
-  password: process.env.DB_PASSWORD,  
-  database: process.env.DB_NAME,   
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false } 
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
